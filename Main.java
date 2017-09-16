4c3c5fdbb98342fe8f565a26eb82cde03a4d5ef9
venkateshwarreddy0119 <32011183+venkateshwarreddy0119@users.noreply.github.com>
2017-09-16 03:40:29 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		//StudentGroup s = new StudentGroup(1);
+		//Student s1=new Student(1,"abc",new Date(),10);
+		//s.addFirst(s1);
+		//System.out.println(s.getStudent(0));
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+	}
+
+}
